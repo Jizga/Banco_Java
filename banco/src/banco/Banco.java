358eaff4948591ab72a3206c4eb766390dec2b6b
Jizga <jizga93@gmail.com>
2021-02-10 18:28:39 +0100

    1

--- banco/src/banco/Banco.java
+++ banco/src/banco/Banco.java
@@ -5,8 +5,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.PrintWriter;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import cuentas.CuentaCorriente;
